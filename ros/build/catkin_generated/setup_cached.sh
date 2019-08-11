a77007651f23ea966e53b3cd0a1698a00694be46
Parallels <davidyy@umich.edu>
2019-08-11 16:56:18 -0700

    finished dbw node

---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD="/home/parallels/CarND-Capston/ros/build"
+export ROS_PACKAGE_PATH="/home/parallels/pnc/CarND-Capstone/ros/src:/home/parallels/CarND-Capston/ros/src:/opt/ros/melodic/share"
---
+++ ros/devel/share/gennodejs/ros/styx_msgs/msg/_index.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let Waypoint = require('./Waypoint.js');
+let TrafficLightArray = require('./TrafficLightArray.js');
+let TrafficLight = require('./TrafficLight.js');
+let Lane = require('./Lane.js');
+
+module.exports = {
+  Waypoint: Waypoint,
+  TrafficLightArray: TrafficLightArray,
+  TrafficLight: TrafficLight,
+  Lane: Lane,
+};
